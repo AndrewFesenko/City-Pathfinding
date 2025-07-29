f8d316613b0a161c91b64464d87d97387fe3ca34
AndrewFesenko <andrewfesenko10@gmail.com>
2025-07-29 18:05:49 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/City_Pathfinding.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/City_Pathfinding.dir/main.cpp.obj"
+  "CMakeFiles/City_Pathfinding.dir/main.cpp.obj.d"
+  "City_Pathfinding.exe"
+  "City_Pathfinding.exe.manifest"
+  "City_Pathfinding.pdb"
+  "libCity_Pathfinding.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/City_Pathfinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

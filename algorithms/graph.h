5101e2d3529bc3a45fc9247e6735de20ac1dfe79
Ethan Taylor <taylorethan@ufl.edu>
2025-08-02 15:15:51 -0400

    Added nodeFound method, re-factored IDs to accomodate bigger numbers; int -> long long

--- algorithms/graph.h
+++ algorithms/graph.h
@@ -10,7 +10,7 @@ using namespace std;
 
 // represents a connection (aka road) from one node to another
 struct Edge {
-    int to; // destination
+    long long to; // destination
     double cost; // cost of traveling (time, distance, etc. etc.)
     bool isOpen = true; // if false, the edge is closed ( so this is like us simulating the road blocked)
     double trafficMultiplier = 1.0; // multipler for cost due to traffic (this is so we can track how traffic affects the cost so like 1.0 is normal, 2.0 is heavy)
@@ -18,7 +18,7 @@ struct Edge {
 
 // for nodes in city (aka intersections)
 struct Node {
-    int id; // id of node
+    long long id; // id of node
     double x = 0.0, y = 0.0; // could remove this later, this is for A* ("A* algorithm uses heuristics to guide its search") so this is for the heuristics os lat/long or whatever metrics
 };
 
@@ -27,14 +27,20 @@ public:
     unordered_map<int, Node> nodes; //maps nodes to node struct
     unordered_map<int, vector<Edge>> adj; // node id to edge
 
-    void addNode(int id, double x = 0.0, double y = 0.0) {
+    void addNode(long long id, double x = 0.0, double y = 0.0) {
         nodes[id] = Node{id, x, y}; // this creates an intersection in graph and assigns id (ex. use case: g.addNode(42, 3.2, 5.7) interesction 42 is at (3.2, 5.7) (mainly for a*)
     } //currently for hardcode testing coordinates and tiny graphs fill need to change later to read file or csv with like while (file >> id >> x >> y){ g.addNode(id, x, y)}
     // same for edges below g.addEdge(from, to, cost, isOpen, traffic);
 
-    void addEdge(int from, int to, double cost, bool open = true, double traffic = 1.0) { // adds two way edge to nodes
+    void addEdge(long long from, long long to, double cost, bool open = true, double traffic = 1.0) { // adds two way edge to nodes
         adj[from].push_back({to, cost, open, traffic});
         adj[to].push_back({from, cost, open, traffic}); // undirected
+    }
+
+    bool nodeFound(long long id) {
+        auto it = nodes.find(id);
+        if(it == scores.end()) return false;
+        return true;
     }
 };
 

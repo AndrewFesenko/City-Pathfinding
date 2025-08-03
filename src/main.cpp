f2b21344f1ba7d9c94008ef4d805b5aca477b39c
Ethan Taylor <taylorethan@ufl.edu>
2025-08-02 22:22:19 -0400

    Main.cpp accepts standard input. Markdown updated. Command-line functionality missing. Locations.md file missing.

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,19 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <thread>
 
 using namespace std;
 
 // helper to print path
-void printPath(const string& name, const vector<int>& path) {
+void printPath(const string& name, const vector<long long>& path) {
 	cout << name << " Path: ";
 	for (int node : path) cout << node << " ";
 	cout << "\n";
 }
 
 // prints path cost
-double getPathCost(const Graph& g, const vector<int>& path) {
+double getPathCost(const Graph& g, const vector<long long>& path) {
 	double cost = 0;
 	for (size_t i = 0; i < path.size() - 1; ++i) {
 		int from = path[i];
@@ -39,6 +40,25 @@ int main() {
 	string csv_path = "../data/city_edges_with_traffic.csv";
 
 	ifstream file(csv_path);
+	cout << " _______ __________________              _______  _______ _________            _______ _________ _        ______  _________ _        _______  _ " << endl;
+	cout << R"((  ____ \\__   __/\__   __/|\     /|    (  ____ )(  ___  )\__   __/|\     /|  (  ____ \\__   __/( (    /|(  __  \ \__   __/( (    /|(  ____ \( ))" << endl;
+	cout << R"(| (    \/   ) (      ) (   ( \   / )    | (    )|| (   ) |   ) (   | )   ( |  | (    \/   ) (   |  \  ( || (  \  )   ) (   |  \  ( || (    \/| |)" << endl;
+	cout << R"(| |         | |      | |    \ (_) /     | (____)|| (___) |   | |   | (___) |  | (__       | |   |   \ | || |   ) |   | |   |   \ | || |      | |)" << endl;
+	cout << R"(| |         | |      | |     \   /      |  _____)|  ___  |   | |   |  ___  |  |  __)      | |   | (\ \) || |   | |   | |   | (\ \) || | ____ | |)" << endl;
+	cout << R"(| |         | |      | |      ) (       | (      | (   ) |   | |   | (   ) |  | (         | |   | | \   || |   ) |   | |   | | \   || | \_  )(_))" << endl;
+	cout << R"(| (____/\___) (___   | |      | |       | )      | )   ( |   | |   | )   ( |  | )      ___) (___| )  \  || (__/  )___) (___| )  \  || (___) | _ )" << endl;
+	cout << R"() (_______/\_______/   )_(      \_/       |/       |/     \|   )_(   |/     \|  |/       \_______/|/    )_)(______/ \_______/|/    )_)(_______)(_))" << endl << endl;
+	// Generated with the assistance of Claude Sonnet 4 AI
+	cout << "Welcome to City Pathfinding!" << endl;
+	string user_from;
+	cout << "Where from? ";
+	cin >> user_from;
+
+	string user_to;
+	cout << "Where to? ";
+	cin >> user_to;
+
+
 
 	if(!file.is_open()) {
 		cerr << "Error: Could not find and open file " << csv_path << endl;
@@ -49,6 +69,7 @@ int main() {
 	int lineNumber = -1;
 
 	cout << "Reading data..." << endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
 	while(getline(file,line)) {
 		lineNumber++;
@@ -87,35 +108,38 @@ int main() {
 		
 
 	}
-	cout << "Data successfully read!" << endl;
+	cout << "Data successfully read!" << endl << endl;
 
 	
 
-	// auto startDijkstra = chrono::high_resolution_clock::now();
-	// auto dijkstraPath = dijkstra(g, 11386675172, 123198914);		// this says g (graph) runs 1 through 4
-	// auto endDijkstra = chrono::high_resolution_clock::now();
-	// auto durationDijkstra = chrono::duration_cast<chrono::microseconds>(endDijkstra - startDijkstra).count();
+	cout << "Applying Dijkstra's Algorithm..." << endl << endl;
+	auto startDijkstra = chrono::high_resolution_clock::now();
+	auto dijkstraPath = dijkstra(g, stoll(user_from), stoll(user_to));
+	auto endDijkstra = chrono::high_resolution_clock::now();
+	auto durationDijkstra = chrono::duration_cast<chrono::microseconds>(endDijkstra - startDijkstra).count();
 
-	// auto startAStar = chrono::high_resolution_clock::now();
-	// auto astarPath = astar(g, 1, 4);
-	// auto endAStar = chrono::high_resolution_clock::now();
-	// auto durationAStar = chrono::duration_cast<chrono::microseconds>(endAStar - startAStar).count();
+	cout << "Applying A* Algorithm..." << endl << endl;
+	auto startAStar = chrono::high_resolution_clock::now();
+	auto astarPath = astar(g, stoll(user_from), stoll(user_to));
+	auto endAStar = chrono::high_resolution_clock::now();
+	auto durationAStar = chrono::duration_cast<chrono::microseconds>(endAStar - startAStar).count();
 
-	// printPath("Dijkstra", dijkstraPath);
-	// if (!dijkstraPath.empty()) {
-	// 	cout << "  Total Cost: " << getPathCost(g, dijkstraPath) << "\n";
-	// 	cout << "  Time Taken: " << durationDijkstra << " us\n";
-	// } else {
-	// 	cout << "  No path found.\n";
-	// }
 
-	// printPath("A*", astarPath);
-	// if (!astarPath.empty()) {
-	// 	cout << "  Total Cost: " << getPathCost(g, astarPath) << "\n";
-	// 	cout << "  Time Taken: " << durationAStar << " us\n";
-	// } else {
-	// 	cout << "  No path found.\n";
-	// }
+	printPath("Dijkstra", dijkstraPath);
+	if (!dijkstraPath.empty()) {
+		cout << "  Total Cost: " << getPathCost(g, dijkstraPath) << "\n";
+		cout << "  Time Taken: " << durationDijkstra << " us\n" << endl;
+	} else {
+		cout << "  No path found.\n";
+	}
+
+	printPath("A*", astarPath);
+	if (!astarPath.empty()) {
+		cout << "  Total Cost: " << getPathCost(g, astarPath) << "\n";
+		cout << "  Time Taken: " << durationAStar << " us\n" << endl;
+	} else {
+		cout << "  No path found.\n";
+	}
 
 	return 0;
 }
